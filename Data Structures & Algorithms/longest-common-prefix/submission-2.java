16837fcd694e6bf4d2417eefcc99be5e7d13c5d4
RidhimaGoyal45 <ridhimagoyal161@gmail.com>
2026-08-20 22:56:07 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String s = strs[0];
+        int n = s.length();
+        for(int i=1;i<strs.length;i++){
+            while(!strs[i].startsWith(s)){
+                s = s.substring(0,n-1);
+                n = s.length();
+                if(n==0){
+                    return "";
+                }
+            }
+        }
+        return s;
+    }
+}
